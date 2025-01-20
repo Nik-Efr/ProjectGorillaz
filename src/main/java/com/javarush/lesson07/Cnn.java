06c1e9d680bf4aa84c7b93b51211892cc4f4ffda
akhmelev <akhmelev@gmail.com>
2025-01-20 21:07:51 +0300

    ADD JDBC demo

---
+++ src/main/java/com/javarush/lesson07/Cnn.java
@@ -0,0 +1,29 @@
+package com.javarush.lesson07;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Cnn {
+
+    public static final String DB_URI_KEY = "db.uri";
+    public static final String DB_USER = "db.user";
+    public static final String DB_PASSWORD = "db.password";
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection get() throws SQLException {
+        return DriverManager.getConnection(
+                Configuration.getProperty(DB_URI_KEY),
+                Configuration.getProperty(DB_USER),
+                Configuration.getProperty(DB_PASSWORD)
+        );
+    }
+
+}
